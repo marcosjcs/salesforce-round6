50b4b21b3fd1f3c7a99d3817aeff0db3e11ed110
marcosjcs <jcs.marcos13@gmail.com>
2021-12-12 07:27:40 -0300

    Refatoração atributo container

--- force-app/main/default/lwc/atributoContainer/atributoContainer.js
+++ force-app/main/default/lwc/atributoContainer/atributoContainer.js
@@ -1,12 +1,10 @@
 import { LightningElement, api } from 'lwc';
 
 export default class AtributoContainer extends LightningElement {
-  @api atributoValue;
-  @api atributoLabel;
-  @api atributoColor
+  @api atributo
 
   get atributoClass() {
-    return `width:${this.atributoValue}%;background:`+this.atributoColor;
+    return `width:${this.atributo.value}%;background:`+this.atributo.color;
   }
 
 }
